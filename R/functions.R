447e463e0f9b905714edc2af50cc388ee1af8e3e
Tua Gyldenholm <tua.gyldenholm@gmail.com>
2023-11-09 10:59:10 +0100

    Created plot function

--- R/functions.R
+++ R/functions.R
@@ -10,3 +10,16 @@ descriptive_stats <- function(data) {
         dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
         dplyr::mutate(dplyr::across(dplyr::where(is.numeric), ~ round(.x, digits = 1)))
 }
+
+#' plot_distributions
+#'
+#' @param data
+#'
+#' @return A plot object.
+
+plot_distributions <- function(data) {
+    metabolite_distribution_plot <- ggplot2::ggplot(data, aes(x = value)) +
+        geom_histogram() +
+        facet_wrap(vars(metabolite), scales = "free")
+    metabolite_distribution_plot
+}
